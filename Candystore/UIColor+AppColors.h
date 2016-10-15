3384a369a7c0c95e1eeed5e3eba6658453c1f427
g20ready <tmarselg@gmail.com>
2016-10-15 12:13:32 +0300

    App colors

---
+++ Candystore/UIColor+AppColors.h
@@ -0,0 +1,20 @@
+//
+//  AppColors.h
+//  Candystore
+//
+//  Created by Marsel Xhaxho on 15/10/2016.
+//  Copyright © 2016 max@dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (AppColors)
+
++ (UIColor*) lightGreen;
++ (UIColor*) darkGreen;
++ (UIColor*) cyan;
++ (UIColor*) bordeaux;
++ (UIColor*) pink;
+
+
+@end
---
+++ Candystore/UIColor+AppColors.m
@@ -0,0 +1,49 @@
+//
+//  AppColors.m
+//  Candystore
+//
+//  Created by Marsel Xhaxho on 15/10/2016.
+//  Copyright © 2016 max@dev. All rights reserved.
+//
+
+#import "UIColor+AppColors.h"
+
+@implementation UIColor(AppColors)
+
++ (UIColor*) lightGreen {
+    return [UIColor colorWithRed:91.0/255.0
+                           green:198.0/255.0
+                            blue:155.0/255.0
+                           alpha:1.0];
+}
+
++ (UIColor*) darkGreen {
+    return [UIColor colorWithRed:91.0/255.0
+                           green:198.0/255.0
+                            blue:155.0/255.0
+                           alpha:1.0];
+}
+
++ (UIColor*) cyan {
+    return [UIColor colorWithRed:143.0/255.0
+                           green:255.0/255.0
+                            blue:210.0/255.0
+                           alpha:1.0];
+}
+
++ (UIColor*) bordeaux {
+    return [UIColor colorWithRed:122.0/255.0
+                           green:46.0/255.0
+                            blue:31.0/255.0
+                           alpha:1.0];
+}
+
++ (UIColor*) pink {
+    return [UIColor colorWithRed:198.0/255.0
+                           green:115.0/255.0
+                            blue:99.0/255.0
+                           alpha:1.0];
+}
+
+
+@end
