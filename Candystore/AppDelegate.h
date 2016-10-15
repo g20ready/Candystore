d6af27c005dd8cf774ed1b1edd1e7a7fcc54c960
g20ready <tmarselg@gmail.com>
2016-10-15 11:50:43 +0300

    Initial commit

---
+++ Candystore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Candystore
+//
+//  Created by Marsel Xhaxho on 15/10/2016.
+//  Copyright © 2016 max@dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Candystore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Candystore
+//
+//  Created by Marsel Xhaxho on 15/10/2016.
+//  Copyright © 2016 max@dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
